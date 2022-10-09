c44a83bebc5ac4a086a75b52291035481616ef14
felipeperaltacheung <f33___@hotmail.com>
2022-10-09 14:22:48 -0300

    Uso de Heranca

---
+++ bytebank/src/Gerente.java
@@ -0,0 +1,21 @@
+public class Gerente extends Funcionario{ //gerente herda um funcionario
+
+    private int senha;
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha){
+        if (this.senha == senha){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+//    public double getBonificacao(){
+//        return this.salario;
+//    }
+
+}

1a68b1447babf84b807f51f634d31a97eb429b71
felipeperaltacheung <f33___@hotmail.com>
2022-10-11 08:18:18 -0300

    Construtores com super

--- bytebank/src/Designer.java
+++
@@ -1,8 +0,0 @@
-public class Designer extends Funcionario{ //gerente herda um funcionario
-
-    public double getBonificacao(){
-        System.out.println("metodo de boni do designer");
-        return 200;
-    }
-
-}
--- bytebank/src/EditorVideo.java
+++
@@ -1,8 +0,0 @@
-public class EditorVideo extends Funcionario{ //gerente herda um funcionario
-
-    public double getBonificacao(){
-        System.out.println("metodo de boni do editor");
-        return super.getBonificacao() + 100;
-    }
-
-}
